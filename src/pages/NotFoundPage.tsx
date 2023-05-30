4180259457b0932c821b2fe15510ad45188d0882
Victor Reis <victorsireis@gmail.com>
2023-05-30 17:45:20 -0300

    feat: adding prettier and react router dom

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Página não encontrada</h1>
+      <p>Desculpe, a página que você está tentando visualizar não existe.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
